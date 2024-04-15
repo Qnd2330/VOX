e2cf881e51132eb996dc2f40387680c4406838ec
Qnd2330 <quachduc1423@gmail.com>
2024-04-15 15:40:36 +0700

    DAO

---
+++ VOX_BE/src/main/java/VOX_Giat_La/Repositories/UserRepos.java
@@ -0,0 +1,7 @@
+package VOX_Giat_La.Repositories;
+
+import VOX_Giat_La.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepos extends JpaRepository<User,Integer> {
+}
